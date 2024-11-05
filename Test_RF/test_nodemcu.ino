ba188f261a929f5e80308cda8bf1f005b9955383
Jakub Rolný <121765270+rojagth@users.noreply.github.com>
2024-11-05 08:55:08 +0100

    Update test_nodemcu.ino

--- Test_RF/test_nodemcu.ino
+++ Test_RF/test_nodemcu.ino
@@ -3,15 +3,15 @@
 #include <RF24.h>
 
 RF24 radio(D2, D1);  // CE, CSN pins on NodeMCU
-const byte address[6] = "00001";  // Unique address for communication
+const byte address[6] = "00001";
 
 void setup() {
   Serial.begin(9600);
   radio.begin();
   radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_LOW);   // Start with low power
-  radio.setDataRate(RF24_1MBPS);   // Set data rate to 1Mbps for a balanced test
-  radio.stopListening();           // Set as transmitter
+  radio.setPALevel(RF24_PA_LOW);
+  radio.setDataRate(RF24_1MBPS);
+  radio.stopListening();
 }
 
 void loop() {
@@ -22,5 +22,5 @@ void loop() {
   } else {
     Serial.println("Message failed to send");
   }
-  delay(1000);  // Send message every second
+  delay(1000);
 }
